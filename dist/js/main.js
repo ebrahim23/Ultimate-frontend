292eb2b3b5de1376a740638fe48d2038c4e094ca
Ibrahim Khalid <43196067+ebrahim23@users.noreply.github.com>
2020-04-28 23:03:28 +0300

    done

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,10 +2,11 @@ const myMap = new Map();
 const mySet = new Set();
 $(function(){
     'use strict';
+    // Toggle Sidebar
     $('.side-toggling').on('click', function(){
         $('.content, .sidebar').toggleClass('no-side');
     });
-    // Toggle submenu
+    // Toggle Submenu
     $(".arrows").on('click', function(){
         $(this).find('.fa-arrow-circle-right').toggleClass('down');
         $(this).next('.sub-links').slideToggle();
--- stage/js/main.js
+++ stage/js/main.js
@@ -1,9 +1,10 @@
 $(function(){
     'use strict';
+    // Toggle Sidebar
     $('.side-toggling').on('click', function(){
         $('.content, .sidebar').toggleClass('no-side');
     });
-    // Toggle submenu
+    // Toggle Submenu
     $(".arrows").on('click', function(){
         $(this).find('.fa-arrow-circle-right').toggleClass('down');
         $(this).next('.sub-links').slideToggle();
