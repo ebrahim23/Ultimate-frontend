4af4c66b8758cd6297c260d225794b3d1ba72971
Ibrahim Khalid <43196067+ebrahim23@users.noreply.github.com>
2020-04-29 15:07:00 +0300

    done

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ultimate-frontend
 ultimate-frontend-course
+
+'With many skills i have this site is made up'
+
+MY SKILLS:
+    01_ HTML / HTML5
+    02_ CSS / CSS3
+    03_ JavaScript
+    04_ JQuery
+    05- Bootstrap
+    06_ Sass
+    07_ Pug
+    08_ Gulp
+    09_ Command Line
+    10_ Git / Github
+    11_ Python
+    12_ WireFraming / ProtoTyping
+
+" From my skills you should noticed that i'm a Front-End Dev "
+
+* You can hiring me and enjoyng with awesome and professional work
--- dist/js/main.js
+++ dist/js/main.js
@@ -11,4 +11,40 @@ $(function(){
         $(this).find('.fa-arrow-circle-right').toggleClass('down');
         $(this).next('.sub-links').slideToggle();
     })
+    // Full Screen
+    $(".fullscreen").on('click', function(){
+        $(this).toggleClass('full-screen')
+        if($(this).hasClass('full-screen')) {
+            openScreen();
+        }else {
+            closeScreen();
+        }
+    })
+    var elem = document.documentElement;
+
+    /* View in fullscreen */
+    function openScreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+    }
+
+    /* Close fullscreen */
+    function closeScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+    }
 })
